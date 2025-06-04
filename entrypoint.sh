5935dbd1a84b40f6ac93b93b30840cf13d569081
Rualin <fateevrual@gmail.com>
2025-06-04 16:44:06 +0700

    Revert "Added unworking kubernetes" Reason: Commit was pushed to incorrect branch This reverts commit 3f19d259859b7947916c3dd15eb688a1a2706011.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Получаем имя пода с тестами
-APP_POD=$(kubectl get pods -l app=candle-app -o jsonpath="{.items[0].metadata.name}")
-
-# Получаем логи и сохраняем в HTML
-kubectl logs $APP_POD > /usr/share/nginx/html/index.html
+# Получаем логи тестов из контейнера app
+docker logs test-app > /usr/share/nginx/html/index.html
 
 # Запускаем Nginx
 nginx -g "daemon off;"
--- entrypoint_old.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Получаем логи тестов из контейнера app
-docker logs test-app > /usr/share/nginx/html/index.html
-
-# Запускаем Nginx
-nginx -g "daemon off;"
